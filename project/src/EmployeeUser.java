71a1a4e9350f6c21ecbcd1988a819c2436b31c1e
kengit1 <es.akram.saad2023@alexu.edu.eg>
2025-10-17 22:48:12 +0300

    Done the : AbstractDatabase Record of lineRep.& getSearchKey Employee Role

---
+++ project/src/EmployeeUser.java
@@ -0,0 +1,68 @@
+public class EmployeeUser implements Record {
+    private final String employeeId,name ;
+    private String email,address,phoneNumber ;
+    EmployeeUser(String employeeId , String name , String email, String address , String phoneNumber)
+    {
+        this.employeeId = employeeId ;
+        this.name = name ;
+        this.email = email ;
+        this.address =address;
+        this.phoneNumber = phoneNumber ;
+    }
+    // setters
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    // getters
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static void main(String[] args) //test main
+    {
+        EmployeeUser user = new EmployeeUser("Emp101","AkrmSaad","akrSad@gmail.com",
+                "15 street street","01090132139") ;
+        EmployeeUserDatabase database = new EmployeeUserDatabase("EmployeeUserDatabase.txt") ;
+        database.readFromFile();
+        database.insertRecord(user);
+        database.saveToFile();
+        System.out.println(database.returnAllRecords());
+    }
+
+    @Override
+    public String toString()
+    {
+        return employeeId+","+name+","+email+","+address+","+phoneNumber ;
+    }
+    @Override
+    public String lineRepresentation() {
+        return this.toString() ;
+    }
+
+    @Override
+    public String getSearchKey() {
+        return employeeId;
+    }
+}
